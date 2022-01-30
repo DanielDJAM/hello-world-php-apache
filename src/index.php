fafab989696713d83a7c27cb362bc29fabf61e8c
DanielDJAM <55432788+DanielDJAM@users.noreply.github.com>
2022-01-30 16:37:13 +0000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        echo ("Bienvenidos a la página de Daniel DJAM");
-    ?>
-</body>
-</html>
+<?php
+    echo "<h1>Hola mundo, esta es la página de Daniel<h1>";
+    echo "<img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eldia.es%2Ftenerife%2F2022%2F01%2F27%2Fcabildo-tenerife-descarta-guaguas-acceder-nieve-teide-62018241.html&psig=AOvVaw0bleSJpuk6PULfwlFmq9PZ&ust=1643646970635000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMCS2aH02fUCFQAAAAAdAAAAABAD'>";
+?>
